c2397d43261a2080478d650b06b546ce50d3dbf3
zazemlil <tiknik19@mail.ru>
2025-12-09 00:58:47 +0300

    refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ extern float analize(char* arg);
 int main(int argc, char* argv[])
 {   
     std::cout << "-----------------------------\n";
-    std::cout << "----Abstract syntax tree:----\n";
+    std::cout << "------------Result:----------\n";
     std::cout << "-----------------------------\n";
     std::cout << analize(argv[1]) << "\n";
     
